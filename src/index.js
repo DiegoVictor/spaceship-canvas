f3f67243037ef987a45414e856841e37aad9b348
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-05-01 20:43:05 -0300

    Show the game's screen

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 import Store from './store';
 import { setScreen } from './store/screen/actions';
 
+import SpaceshipCanvas from './components/SpaceshipCanvas';
+import Screen from './containers/Screen';
+import Background from './components/Background';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+SpaceshipCanvas.bg = new Image();
+SpaceshipCanvas.bg.src = 'img/bg.png';
+
+
+// Insert canvas into HTML body
+ReactDOM.render(
+  <canvas height={550} width={320} ref={e => {
+    SpaceshipCanvas.canvas = e;
+
+    // Retrieve canvas' context and store in the 
+    // SpaceshipCanvas class making the context available 
+    // to all its children components
+    SpaceshipCanvas.ctx = e.getContext('2d');
+  }} />,
+  document.getElementById('app')
+);
+
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <Screen>
+      <Background loop="infinite" />
+    </Screen>
+  </Provider>,
+  SpaceshipCanvas.canvas
+);
