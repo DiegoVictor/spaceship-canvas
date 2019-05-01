8f0cc9a9b12e87b5d5fb14352d949f8f63389c8b
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-05-01 20:36:41 -0300

    Create project's super component

---
+++ src/components/SpaceshipCanvas.js
@@ -0,0 +1,31 @@
+'use script';
+
+import React from 'react';
+
+class SpaceshipCanvas extends React.Component {
+  render() {
+    let ctx;
+
+    /* Get context if it is setted */
+    if (typeof SpaceshipCanvas.ctx !== 'undefined') {
+      ctx = SpaceshipCanvas.ctx;
+    }
+
+    /* Call draw only whether it is setted */
+    if (typeof this.draw === 'function') {
+      let component = this.draw(ctx);
+      if (component) { /* case draw return a component */
+        return component;
+      }
+    }
+
+    /* Whether a component has children components render it */
+    if (typeof this.props.children !== 'undefined') {
+      return (this.props.children);
+    }
+
+    return null;
+  }
+}
+
+export default SpaceshipCanvas;
