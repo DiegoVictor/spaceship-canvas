a6610a355e51ce5b291138d631b3854dd3df542b
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-07-09 20:18:29 -0300

    Move and attach listeners

---
+++ src/containers/screens/Game.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Screen from './Screen';
+
+import Background from '../Background';
+import Enemies from '../Enemies';
+import Group from '../Group';
+import Lifebar from '../Lifebar';
+import Multiplier from '../Multiplier';
+import Score from '../Score';
+import Shoots from '../Shoots';
+import Spaceship from '../Spaceship';
+
+import { setStatus } from '../../store/actions/game';
+import { toggleKey } from '../../store/actions/keyboard';
+import { toggleMovementSpeed } from '../../store/actions/spaceship';
+
+import { HEIGHT, WIDTH } from '../../globals';
+
+
+class Game extends Screen {
+  constructor(props) {
+    super(props);
+    ['keydown', 'keyup'].forEach(event_name => {
+      this.attach(event_name, event => {
+        const key = event.key.replace('Arrow', '');
+        let pressing = true;
+
+        if (event.type === 'keyup') {
+          pressing = false;
+        }
+  
+        if (!props.keyboard[key] || !pressing) {
+          switch (key) {
+            case 'Shift':
+              props.toggleMovementSpeed(pressing);
+              break;
+  
+            default:
+              if (['z', 'Up', 'Right', 'Down', 'Left'].indexOf(key) > -1) {
+                props.toggleKey(key, pressing);
+              }
+          }
+        }
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.props.setStatus('playing');
+  }
+
+  render() {
+    return (<Group>
+      <Background />
+
+      {/* Player's hud */}
+      <Lifebar x={8} y={10} />
+      <Multiplier x={WIDTH - 8} y={HEIGHT - 24} />
+      <Score x={WIDTH - 8} y={10} />
+
+      <Spaceship />
+
+      {/* Draw player and enemies' shoots */}
+      <Shoots />
+
+      {/* Draw enemies */}
+      <Enemies />
+    </Group>);
+  }
+}
+
+export default connect(
+  state => ({
+    keyboard: state.keyboard,
+  }),
+  { toggleKey, toggleMovementSpeed, setStatus }
+)(Game);
---
+++ src/containers/screens/Loading.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Background from '../Background';
+import Group from '../Group';
+import Progress from '../Progress';
+import Text from '../../components/Text';
+
+import { setStatus } from '../../store/actions/game';
+
+import { HEIGHT, WIDTH } from '../../globals';
+
+class Loading extends React.Component {
+  componentDidMount() {
+    this.props.setStatus('loading');
+  }
+
+  render() {
+    return (<Group>
+      <Background />
+      {/* Set the position and the number of files to be loaded */}
+      <Progress x={WIDTH / 2 - 55} y={HEIGHT / 2}
+        process={3} oncomplete={this.props.oncomplete}
+      />
+      <Text value="Loading" x={WIDTH / 2} y={HEIGHT / 2 - 10} />
+    </Group>);
+  }
+}
+
+export default connect(
+  null,
+  { setStatus }
+)(Loading);
