cea83f6a81fae0713aec4e1c3bbb260d3f681cfc
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-07-09 20:25:35 -0300

    Fix indentation

--- src/containers/Spaceship.js
+++ src/containers/Spaceship.js
@@ -3,13 +3,13 @@ import { connect } from 'react-redux';
 
 /* Player spaceship */
 class Spaceship extends SpaceshipCanvas {
-	draw(ctx) {
-		ctx.drawImage(
-			SpaceshipCanvas.spritesheet, 53, 26, 32, 21, 
+  draw(ctx) {
+    ctx.drawImage(
+      SpaceshipCanvas.spritesheet, 53, 26, 32, 21, 
       this.props.x - this.props.width / 2, this.props.y,
       this.props.width, this.props.height
-		);
-	}
+    );
+  }
 }
 
 export default connect(state => ({
