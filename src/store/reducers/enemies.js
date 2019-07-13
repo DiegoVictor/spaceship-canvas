e822c1afac247df69b37a3a49082521cd09ec4f8
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-07-13 20:18:14 -0300

    Remove enemies out of the screen

--- src/store/reducers/enemies.js
+++ src/store/reducers/enemies.js
@@ -1,3 +1,4 @@
+import { HEIGHT } from '../../globals';
 import { MOVE_ENEMIES, REMOVE_ALL_ENEMIES, REMOVE_ENEMY } from '../action_types';
 
 const initial_state = [];
@@ -7,11 +8,15 @@ export default (state = initial_state, action) => {
   switch(action.type) {
     case MOVE_ENEMIES:
       enemies = Object.assign([], state);
-      enemies = enemies.map(enemy => {
-        enemy.y += enemy.step;
-        enemy.rotation += Math.PI / 180 * enemy.step;
-        return enemy;
-      });
+
+      for(let i in enemies) {
+        enemies[i].y += enemies[i].step;
+        if (enemies[i].y > HEIGHT) {
+          enemies.splice(i--, 1);
+          continue;
+        }
+        enemies[i].rotation += Math.PI / 180 * enemies[i].step;
+      }
       return enemies;
     
     case REMOVE_ALL_ENEMIES:
